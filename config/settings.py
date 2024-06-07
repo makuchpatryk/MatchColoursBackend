2a364cac9a074e307f6003907dbdd1affd9d2ce9
Pat <makuchpatryk@gmail.com>
2024-06-07 16:34:36 +0200

    first commit 9

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'USER': str(os.getenv('POSTGRES_USER')),
         'PASSWORD': str(os.getenv('POSTGRES_PASSWORD')),
         'HOST': str(os.getenv('POSTGRES_HOST')),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'PORT': int(os.getenv('POSTGRES_PORT')),
     }
 }
 
