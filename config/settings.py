ca8807c8175eb2132316726407b63a1d5965c67e
Pat <makuchpatryk@gmail.com>
2024-06-07 15:19:57 +0200

    first commit

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECREAT_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', ".now.sh"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
